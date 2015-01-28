1f513d4d6b16cad7dbef98b65aa326eb4280725b
jmarkkula <julia.markkula@gmail.com>
2015-01-28 03:59:50 +0200

    create ja drop tables ja testidataa

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,23 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+
+-- Kayttaja-taulun testidata
+INSERT INTO Kayttaja (nimimerkki, email) VALUES ('niku', 'niku@on.best');
+INSERT INTO Kayttaja (nimimerkki, email) VALUES ('juli', 'juli@on.kans');
+
+-- Resepti-taulun testidata
+INSERT INTO Resepti (nimi, valmistusohje, lisaaja) VALUES ('Pasta Carbonaran kastike', 'Paista pekoni ja valkosipuli, lisää kerma.', 'niku');
+
+-- Raakaaine-taulun testidata
+INSERT INTO Raakaaine (nimi) VALUES ('Pekoni');
+INSERT INTO Raakaaine (nimi) VALUES ('Kerma');
+INSERT INTO Raakaaine (nimi) VALUES ('Valkosipuli');
+
+-- Ainesosa-taulun testidata
+INSERT INTO Ainesosa (raakaaine, maara, reseptitunnus) VALUES ('Pekoni', '500g', 1);
+INSERT INTO Ainesosa (raakaaine, maara, reseptitunnus) VALUES ('Kerma', '2 dl', 1);
+INSERT INTO Ainesosa (raakaaine, maara, reseptitunnus) VALUES ('Valkosipuli', '4 kynttä', 1);
+
+-- Kategoria-taulun testidata
+INSERT INTO Kategoria (nimi, reseptitunnus) VALUES ('herkut', 1);
+
+-- Suosikkiresepti-taulun testidata
+INSERT INTO Suosikkiresepti (reseptitunnus, kayttaja) VALUES (1, 'juli');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,37 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+nimimerkki varchar(20) PRIMARY KEY,
+email varchar(320)
+);
+
+CREATE TABLE Resepti(
+tunnus SERIAL PRIMARY KEY,
+nimi varchar(50) NOT NULL,
+valmistusohje varchar NOT NULL,
+lisaaja varchar(20) REFERENCES Kayttaja(nimimerkki)
+);
+
+CREATE TABLE Raakaaine(
+nimi varchar(20) PRIMARY KEY,
+kuvaus varchar
+);
+
+CREATE TABLE Ainesosa(
+raakaaine varchar(20) NOT NULL REFERENCES Raakaaine(nimi),
+maara varchar NOT NULL,
+reseptitunnus integer NOT NULL REFERENCES Resepti(tunnus),
+PRIMARY KEY (raakaaine, maara, reseptitunnus)
+);
+
+
+CREATE TABLE Kategoria(
+nimi varchar NOT NULL,
+reseptitunnus integer NOT NULL REFERENCES Resepti(tunnus),
+PRIMARY KEY (nimi, reseptitunnus)
+);
+
+CREATE TABLE Suosikkiresepti(
+reseptitunnus integer NOT NULL REFERENCES Resepti(tunnus),
+kayttaja varchar(20) NOT NULL REFERENCES Kayttaja(nimimerkki),
+PRIMARY KEY (reseptitunnus, kayttaja)
+
+);
