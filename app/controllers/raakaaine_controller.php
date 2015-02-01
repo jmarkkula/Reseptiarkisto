1cca77bd3b379a59cb7223a1f10017710b5ef793
jmarkkula <julia.markkula@gmail.com>
2015-02-01 23:00:14 +0200

    raakaaine-viewin aloitus

---
+++ app/controllers/raakaaine_controller.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class RaakaaineController extends BaseController {
+    
+    public static function index() {
+        
+        $raakaaineet = Raakaaine::all();
+        
+        self::render_view('raakaaine/index.html', array('raakaaineet' => $raakaaineet));
+    }
+}
+
